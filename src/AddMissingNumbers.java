88af2dd94616ee9012fd46c6e3e23c736c943035
DESKTOP-HLGL0M7\hyungu.lee <wbsu90@gmail.com>
2022-08-30 21:32:02 +0900

    first commit

---
+++ src/AddMissingNumbers.java
@@ -0,0 +1,19 @@
+public class AddMissingNumbers {
+    public int solution(int[] numbers) {
+
+        int total = sumNaturalNumber(9);
+
+        for(int i : numbers){
+            total -= i;
+        }
+        return total;
+    }
+
+    public int sumNaturalNumber(int finalNumber){
+        if(finalNumber <= 1){
+            return finalNumber;
+        }else {
+            return (finalNumber*(finalNumber+1))/2;
+        }
+    }
+}
